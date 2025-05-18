96ffeb610287826b610e606e8143581eb37b2ad5
RomanSol95 <Romanoflife@Gmail.com>
2025-05-18 17:27:55 +0300

    Add files via upload

---
+++ jshw_3.js
@@ -0,0 +1,56 @@
+//Task 5:
+// const fruits = ["apple", "banana", "cherry", "date"];
+
+// fruits.splice(1, 1, "blueberry", "blackberry");
+
+// console.log(fruits); 
+
+
+//Task 6:
+// const colors = ["red", "orange", "yellow", "green", "blue"];
+
+// const newColors = colors.slice(1, 3);
+
+// console.log(newColors);
+
+
+//Task 7:
+// const animals1 = ["cat", "dog"];
+// const animals2 = ["lion", "tiger"];
+
+// const allAnimals = animals1.concat(animals2);
+
+// console.log(allAnimals); 
+
+
+//Task 8:
+// const numbers = [7, 3, 9, 1, 5];
+
+// numbers.sort((a, b) => a - b);
+
+// console.log(numbers); 
+
+
+//Task 9:
+// const letters = ["a", "b", "c", "d"];
+
+// letters.reverse();
+
+// console.log(letters);
+
+
+//Task 10:
+// const sentence = "JavaScript is awesome";
+
+// const words = sentence.split(" ");
+
+// console.log(words);
+
+
+
+//Task 11:
+// const words = ["Hello", "world"];
+
+// const sentence = words.join(" ");
+
+// console.log(sentence);
